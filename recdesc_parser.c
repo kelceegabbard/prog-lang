7b7589e26cbe27c9981e975c99a4db17ab64b7ed
cpriddy21 <cassandrapriddy21@gmail.com>
2024-03-31 11:12:33 -0500

    init parser

--- recdesc_parser.c
+++ recdesc_parser.c
@@ -1,4 +1,21 @@
+#include <ctype.h>
 #include <stdio.h>
+
+/* character classes */
+#define LETTER 0
+#define DIGIT 1
+#define UNKNOWN 99
+
+/* token codes */
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
 
 /* Global variables */
 int nextToken;
